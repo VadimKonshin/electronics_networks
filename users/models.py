840cd88083e3133534af254b7a0bdd63b6c86985
vadim <vdmkonshin@gmail.com>
2024-10-17 23:07:20 +0300

    new app

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'null':True, "blank":True}
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
+    first_name = models.CharField(max_length=50, verbose_name='Имя', **NULLABLE)
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
